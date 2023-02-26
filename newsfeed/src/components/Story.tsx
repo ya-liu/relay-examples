aab8cce72f755e9f3d7f92cf56278949edbdbecb
Ya Liu <sarahl0216@gmail.com>
2023-02-26 00:26:15 -0800

    useFragment in PosterByline

--- newsfeed/src/components/Story.tsx
+++ newsfeed/src/components/Story.tsx
@@ -15,10 +15,7 @@ const StoryFragment = graphql`
     summary
     createdAt
     poster {
-      name
-      profilePicture {
-        url
-      }
+      ...PosterBylineFragment
     }
     thumbnail {
       url
