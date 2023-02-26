84cdb27f702e31f1b6625b82b413216d9d8327ee
Ya Liu <sarahl0216@gmail.com>
2023-02-26 01:26:41 -0800

    add hover card, use query variables

--- newsfeed/src/components/PosterByline.tsx
+++ newsfeed/src/components/PosterByline.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import Image from "./Image";
+const { useRef } = React;
 import { graphql } from "relay-runtime";
 import { useFragment } from "react-relay";
 import { PosterBylineFragment$key } from "./__generated__/PosterBylineFragment.graphql";
+import Hovercard from "./Hovercard";
+import PosterDetailsHovercardContents from "./PosterDetailsHovercardContents";
 
 const PosterBylineFragment = graphql`
   fragment PosterBylineFragment on Actor {
+    id
     name
     profilePicture {
       ...ImageFragment @arguments(width: 60, height: 60)
@@ -19,11 +23,12 @@ export type Props = {
 
 export default function PosterByline({ poster }: Props): React.ReactElement {
   const data = useFragment(PosterBylineFragment, poster);
+  const hoverRef = useRef(null);
   if (poster == null) {
     return null;
   }
   return (
-    <div className="byline">
+    <div className="byline" ref={hoverRef}>
       <Image
         image={data.profilePicture}
         width={60}
@@ -31,6 +36,9 @@ export default function PosterByline({ poster }: Props): React.ReactElement {
         className="byline__image"
       />
       <div className="byline__name">{data.name}</div>
+      <Hovercard targetRef={hoverRef}>
+        <PosterDetailsHovercardContents posterID={data.id} />
+      </Hovercard>
     </div>
   );
 }
--- newsfeed/src/components/__generated__/NewsfeedQuery.graphql.ts
+++ newsfeed/src/components/__generated__/NewsfeedQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<da9c8a6918facd2173ce498c1dfe1c2c>>
+ * @generated SignedSource<<6dd9b5cd73892a5e3fdaf40e5d49c965>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -120,6 +120,7 @@ return {
                 "kind": "TypeDiscriminator",
                 "abstractKey": "__isActor"
               },
+              (v0/*: any*/),
               {
                 "alias": null,
                 "args": null,
@@ -156,8 +157,7 @@ return {
                   (v1/*: any*/)
                 ],
                 "storageKey": null
-              },
-              (v0/*: any*/)
+              }
             ],
             "storageKey": null
           },
@@ -192,12 +192,12 @@ return {
     ]
   },
   "params": {
-    "cacheID": "561c4317fe6987dfcb257d9dfd671e32",
+    "cacheID": "aebbd0c19babde43569dbd6ea07914e4",
     "id": null,
     "metadata": {},
     "name": "NewsfeedQuery",
     "operationKind": "query",
-    "text": "query NewsfeedQuery {\n  topStories {\n    id\n    ...StoryFragment\n  }\n}\n\nfragment ImageFragment_3XLoCc on Image {\n  url(width: 60, height: 60)\n  altText\n}\n\nfragment ImageFragment_OxVt3 on Image {\n  url(width: 400)\n  altText\n}\n\nfragment PosterBylineFragment on Actor {\n  __isActor: __typename\n  name\n  profilePicture {\n    ...ImageFragment_3XLoCc\n  }\n}\n\nfragment StoryFragment on Story {\n  title\n  summary\n  createdAt\n  poster {\n    __typename\n    ...PosterBylineFragment\n    id\n  }\n  thumbnail {\n    ...ImageFragment_OxVt3\n  }\n}\n"
+    "text": "query NewsfeedQuery {\n  topStories {\n    id\n    ...StoryFragment\n  }\n}\n\nfragment ImageFragment_3XLoCc on Image {\n  url(width: 60, height: 60)\n  altText\n}\n\nfragment ImageFragment_OxVt3 on Image {\n  url(width: 400)\n  altText\n}\n\nfragment PosterBylineFragment on Actor {\n  __isActor: __typename\n  id\n  name\n  profilePicture {\n    ...ImageFragment_3XLoCc\n  }\n}\n\nfragment StoryFragment on Story {\n  title\n  summary\n  createdAt\n  poster {\n    __typename\n    ...PosterBylineFragment\n    id\n  }\n  thumbnail {\n    ...ImageFragment_OxVt3\n  }\n}\n"
   }
 };
 })();
